750073b88a2d54ba6ad27f926e7df65d918e10c1
Aasshhuu8077 <www.ashu12345678@gmail.com>
2024-06-29 16:48:46 +0530

    last commit

--- backend/databse.js
+++ backend/databse.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
     export const connectdb=()=>{
-        mongoose.connect("mongodb://127.0.0.1:27017/petify").then(()=>{
+        mongoose.connect("mongodb+srv://ashutoshbtech2002:<password>@cluster0.uskgvu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
             console.log("Db is connected");
         }).catch((err)=>console.log(err))
     }
